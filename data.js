76124679d625bb4b141bca44f39bc9adf0241e08
Kesler Tanner <keslert@gmail.com>
2013-11-04 19:44:15 -0700

    Added basic game logic

---
+++ data.js
@@ -0,0 +1,389 @@
+var GameData = { 
+	"FamilyFeud": [
+		{
+			"Question": "Name Something You Get At The Dentist That Almost Makes The Trip Worthwhile",
+			"Answers": [
+				{
+					"text": "Toothbrush",
+					"points": 50
+				},{
+					"text": "Candy",
+					"points": 19
+				},{
+					"text": "Clean Teeth",
+					"points": 13
+				},{
+					"text": "Sticker",
+					"points": 8
+				},{
+					"text": "Laughing Gas",
+					"points": 7
+				}
+			]
+		},{
+			"Question": "Tell Me Something You Only Do When You’re Sick",
+			"Answers": [
+				{
+					"text": "Nap",
+					"points": 37
+				},{
+					"text": "Vomit",
+					"points": 34
+				},{
+					"text": "Take Medicine",
+					"points": 14
+				},{
+					"text": "Eat Soup",
+					"points": 8
+				},{
+					"text": "Stay Home From Work",
+					"points": 3
+				},{
+					"text": "Go To Doctor",
+					"points": 3
+				}
+			]
+		},{
+			"Question": "Tell Me A Way In Which The Family Dog Can Annoy You",
+			"Answers": [
+				{
+					"text": "Barking",
+					"points": 38
+				},{
+					"text": "Licking",
+					"points": 23
+				},{
+					"text": "Shedding",
+					"points": 20
+				},{
+					"text": "Scratching",
+					"points": 9
+				},{
+					"text": "Chewing Things",
+					"points": 5
+				}
+			]
+		},{
+			"Question": "Name Something People Consider A Snack Food",
+			"Answers": [
+				{
+					"text": "Chips",
+					"points": 51
+				},{
+					"text": "Popcorn",
+					"points": 12
+				},{
+					"text": "Peanuts",
+					"points": 10
+				},{
+					"text": "Crackers",
+					"points": 8
+				},{
+					"text": "Pretzels",
+					"points": 7
+				},{
+					"text": "Fruit",
+					"points": 6
+				},{
+					"text": "Cookies",
+					"points": 5
+				}
+			]
+		},{
+			"Question": "Name Something Parents Tell Their Kids Not To Waste",
+			"Answers": [
+				{
+					"text": "Food",
+					"points": 53
+				},{
+					"text": "Money",
+					"points": 17
+				},{
+					"text": "Water",
+					"points": 14
+				},{
+					"text": "Electricity",
+					"points": 9
+				},{
+					"text": "Time",
+					"points": 3
+				},{
+					"text": "Life",
+					"points": 3
+				}
+			]
+		},{
+			"Question": "Name A Food Item Served At Most Cafes",
+			"Answers": [
+				{
+					"text": "Cake",
+					"points": 28
+				},{
+					"text": "Sandwich",
+					"points": 27
+				},{
+					"text": "Donut",
+					"points": 15
+				},{
+					"text": "Muffin",
+					"points": 8
+				},{
+					"text": "Pie",
+					"points": 7
+				},{
+					"text": "Bagel",
+					"points": 5
+				}
+			]
+		},{
+			"Question": "Name Something Your Parents Ask About Every Time They See You",
+			"Answers": [
+				{
+					"text": "Work/School",
+					"points": 39
+				},{
+					"text": "Health/Eating Right?",
+					"points": 30
+				},{
+					"text": "Grandkids",
+					"points": 27
+				},{
+					"text": "Partner/Dating anyone?",
+					"points": 3
+				}
+			]
+		},{
+			"Question": "At The Roller Rink, How Can You Identify A Nervous Skater?",
+			"Answers": [
+				{
+					"text": "Wobbly/Falling",
+					"points": 51
+				},{
+					"text": "Holding On To Rail",
+					"points": 28
+				},{
+					"text": "Skating Slowly",
+					"points": 8
+				},{
+					"text": "Stay On Bench",
+					"points": 7
+				},{
+					"text": "Arms Out",
+					"points": 3
+				}
+			]
+		},{
+			"Question": "What Do You Call Someone Who Is Careful With Money?",
+			"Answers": [
+				{
+					"text": "Cheap",
+					"points": 31
+				},{
+					"text": "Stingy",
+					"points": 21
+				},{
+					"text": "Tight",
+					"points": 15
+				},{
+					"text": "Penny Pincher",
+					"points": 12
+				},{
+					"text": "Frugal",
+					"points": 8
+				},{
+					"text": "Scrooge",
+					"points": 6
+				},{
+					"text": "Thrifty",
+					"points": 4
+				}
+			]
+		},{
+			"Question": "Name A Part Of You That Looks Different When You’re Sick",
+			"Answers": [
+				{
+					"text": "Skin",
+					"points": 44
+				},{
+					"text": "Eyes",
+					"points": 34
+				},{
+					"text": "Nose",
+					"points": 18
+				},{
+					"text": "Hair",
+					"points": 3
+				}
+			]
+		},{
+			"Question": "Name A Fruit That’s Delicious To Eat, But Hard To Break Into",
+			"Answers": [
+				{
+					"text": "Coconut",
+					"points": 27
+				},{
+					"text": "Pineapple",
+					"points": 26
+				},{
+					"text": "Watermelon",
+					"points": 22
+				},{
+					"text": "Cantaloupe",
+					"points": 11
+				},{
+					"text": "Pomegranate",
+					"points": 9
+				}
+			]
+		},{
+			"Question": "Name Something You Associate With British People",
+			"Answers": [
+				{
+					"text": "English Accent",
+					"points": 44
+				},{
+					"text": "Drinking Tea",
+					"points": 12
+				},{
+					"text": "The Royal Family",
+					"points": 11
+				},{
+					"text": "Bad Teeth",
+					"points": 10
+				},{
+					"text": "Flag",
+					"points": 4
+				},{
+					"text": "Fish & Chips",
+					"points": 3
+				},{
+					"text": "Snobby Attitude",
+					"points": 3
+				}
+			]
+		},{
+			"Question": "Name A Sport In Which Competitors Wear Funny-Looking Shoes",
+			"Answers": [
+				{
+					"text": "Bowling",
+					"points": 34
+				},{
+					"text": "Soccer",
+					"points": 21
+				},{
+					"text": "Golf",
+					"points": 10
+				},{
+					"text": "Track",
+					"points": 8
+				},{
+					"text": "Basketball",
+					"points": 6
+				},{
+					"text": "Football",
+					"points": 6
+				},{
+					"text": "Wrestling",
+					"points": 4
+				}
+			]
+		},{
+			"Question": "Name Something You Wear That Covers Your Ears",
+			"Answers": [
+				{
+					"text": "Earmuffs",
+					"points": 50
+				},{
+					"text": "Hat",
+					"points": 35
+				},{
+					"text": "Headphones",
+					"points": 5
+				},{
+					"text": "Scarf",
+					"points": 4
+				},{
+					"text": "Hood",
+					"points": 3
+				}
+			]
+		},{
+			"Question": "What One Thing Would You Most Hate To Forget To Pack On A Trip?",
+			"Answers": [
+				{
+					"text": "Clothes",
+					"points": 34
+				},{
+					"text": "Toothbrush",
+					"points": 30
+				},{
+					"text": "Money",
+					"points": 21
+				},{
+					"text": "Camera",
+					"points": 9
+				},{
+					"text": "Passport",
+					"points": 4
+				}
+			]
+		},{
+			"Question": "Name Something You Wear That Covers Your Ears",
+			"Answers": [
+				{
+					"text": "Earmuffs",
+					"points": 50
+				},{
+					"text": "Hat",
+					"points": 35
+				},{
+					"text": "Headphones",
+					"points": 5
+				},{
+					"text": "Scarf",
+					"points": 4
+				},{
+					"text": "Hood",
+					"points": 3
+				}
+			]
+		},{
+			"Question": "Name A Job Where You Can Act Like A Kid",
+			"Answers": [
+				{
+					"text": "Clown",
+					"points": 64
+				},{
+					"text": "Kindergarten Teacher",
+					"points": 11
+				},{
+					"text": "Daycare Provider",
+					"points": 8
+				},{
+					"text": "Actor",
+					"points": 6
+				},{
+					"text": "Toy Store Employee",
+					"points": 6
+				}
+			]
+		},{
+			"Question": "Where Do People Usually Wear Sandals?",
+			"Answers": [
+				{
+					"text": "Beach",
+					"points": 70
+				},{
+					"text": "Pool",
+					"points": 14
+				},{
+					"text": "Park",
+					"points": 9
+				},{
+					"text": "Shower",
+					"points": 4
+				}
+			]
+		}
+	]
+}
--- js/family-feud.js
+++ js/family-feud.js
@@ -1,52 +1,67 @@
 
-keslert.game.FamilyFeud = function(sectionDOM, scoreDOM) {
+function FamilyFeud(questionDOM, sectionDOM, scoreDOM) {
 	this.answers = [];
 
 	this.points = 0;
 	this.stikes = 0;
 
-	this.sectionDOM;
+	this.questionDOM = questionDOM;
+	this.sectionDOM = sectionDOM;
   	this.scoreDOM = scoreDOM;
 
-	this.gameData = JSON.parse('data.json');
+  	this.ifr = document.getElementById('sound');
+  	this.gameData = GameData['FamilyFeud'];
+
+  	this.setUpBuzzers();
+  	this.startRound();
 }
 
-keslert.game.FamilyFeud.prototype.getRoundData = function() {
-	var index = Math.floor(Math.random() * this.gameData['FamilyFeud'].length);
-
-	var round = rounds.splice(index, 1);
-	return round;
+FamilyFeud.prototype.startRound = function() {
+	var roundData = this.getRoundData();
+	this.createBoard(roundData);
 }
 
-keslert.game.FamilyFeud.prototype.createBoard = function() {
+FamilyFeud.prototype.getRoundData = function() {
+	var index = Math.floor(Math.random() * this.gameData.length);
+
+	var round = this.gameData.splice(index, 1);
+	return round[0];
+}
+
+FamilyFeud.prototype.createBoard = function(data) {
+	var me = this;
 	this.sectionDOM.find('section').remove();
 
-	var roundData = getRoundData();
-	var answers = roundData['answers'];
-	var question = roundData['question'];
+	var answers = data['Answers'];
+	var question = data['Question'];
 
-	for(var i = 0; i < 10; i++) {
-		var section;
+	this.questionDOM.text(question);
+	function helper(i) {
 		if(i < answers.length) {
-			section = createSection(answers[i]['text'], answers[i]['points'], i);
-		} else {
-			section = createSection(null, null, i);
+			return me.createSection(answers[i]['text'], answers[i]['points'], i);
 		}
-		this.sectionDOM.append(section);
+		return me.createSection(null, null, i);
+	}
+
+	for(var i = 0; i < 5; i++) {
+		var row = $('<div class="row"></div>');
+		row.append(helper(i));
+		row.append(helper(i+5));
+		this.sectionDOM.append(row);
 	}
 }
 
-keslert.game.FamilyFeud.prototype.createSection = function(answer, points, i) {
-	var section = $('<section id="section_'+i+'"></section');
+FamilyFeud.prototype.createSection = function(answer, points, i) {
+	var section = $('<section class="col-xs-6" id="section_'+i+'"></section');
 	var inner = $('<div></div>');
 	section.append(inner);
 
 	if(answer && points) {
 		section.addClass('active');
 		inner.addClass('answer');
-		inner.append('<figure class="front"><span>'+answer+'</span></figure>');
-		inner.append('<figure class="back"><span>'+points+'</span></figure>');
-		section.o
+		inner.append('<figure class="front"><span>'+(i+1)+'</span></figure>');
+		inner.append('<figure class="back"><div class="left">'+answer+'</div><div class="right"><span>'+points+'</span></div></figure>');
+		this.addClick(section);
 	} else {
 		inner.addClass('inactive');
 	}
@@ -54,18 +69,7 @@ keslert.game.FamilyFeud.prototype.createSection = function(answer, points, i) {
 	return section;
 }
 
-
-$(document).ready(function(){
-	ifr = document.getElementById('sound');
-	sum = 0;
-	strikeCount = 0;
-	
-	setUpAnswers();
-	setUpBuzzers();
-});
-
-
-keslert.game.FamilyFeud.prototype.playSound = function(sound) {
+FamilyFeud.prototype.playSound = function(sound) {
 	var file;
 	if(sound == 'bell') {
 		file = 'ff-clang.wav';
@@ -78,35 +82,34 @@ keslert.game.FamilyFeud.prototype.playSound = function(sound) {
 	}
 }
 
-keslert.game.FamilyFeud.prototype.addClick = function(section) {
+FamilyFeud.prototype.addClick = function(section) {
   var me = this;
 	section.click(function() {
 		var answer = $(this).find('.answer');
 		if (!answer.hasClass('flipped')) {
 			answer.addClass('flipped');
-			me.playBell();
-			me.addScore(section.find('.back span').val());
+			me.playSound('bell');
+			me.addScore(parseInt(section.find('.back span').text()));
 		}
 	})
 }
 
-function setUpBuzzers() {
-	$('#strike').on('click', 
-			function() {
-				if (strikeCount < 3){
-					strikeCount++;
-          $('#strike-count').text(strikeCount);
-					var strike = $('<span class="wrongx">X</span>')
-					var wrong = $('#wrong');
-					wrong.append(strike);
-					playBuzzer();
-					wrong.fadeIn('fast');
-					setTimeout(function() {wrong.fadeOut('fast');}, 1500);
-				}
-			});
+FamilyFeud.prototype.setUpBuzzers = function() {
+	$('#strike').on('click', function() {
+		if (this.strikes < 3) {
+			this.strikes++;
+  			$('#strike-count').text(strikeCount);
+			var strike = $('<span class="wrongx">X</span>')
+			var wrong = $('#wrong');
+			wrong.append(strike);
+			playBuzzer();
+			wrong.fadeIn('fast');
+			setTimeout(function() {wrong.fadeOut('fast');}, 1500);
+		}
+	});
 }
 
-keslert.game.FamilyFeud.prototype.sumScores = function(points) {
+FamilyFeud.prototype.addScore = function(points) {
 	this.points += points;
-	this.scoreDOM.text(sum);
+	this.scoreDOM.text(this.points);
 }
